8c7cb2e4d3d035315d0a1b239949399ee4312dc0
Florian Georg <florian.alexander.georg@gmail.com>
2020-10-09 10:38:22 +0200

    changed routes.json

--- api/BadgeGET/index.js
+++ api/BadgeGET/index.js
@@ -8,21 +8,21 @@ const readFileAsync = util.promisify(fs.readFile);
 
 // Axios is a http client lib
 // see https://github.com/axios/axios#axios-api
-const axios = require('axios'); 
+const axios = require('axios');
 
 const filenameQR = './BadgeGET/builder-qr.png'
 
 module.exports = async function (context, req) {
     context.log('HTTP trigger: GET badge');
 
-    let data = await readFileAsync (filenameQR);
+    let data = await readFileAsync(filenameQR);
 
 
     context.res = {
         status: 200, /* Defaults to 200 */
         headers: {
             'Content-Type': 'image/png'
-        } ,
+        },
         body: data,
     };
 }
